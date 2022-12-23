52930d7f356b25ba16e6556e6f7620057dfc2c41
OscarMoon5256 <oscar.moon5256@gmail.com>
2022-12-23 21:55:55 +0900

    env: Initial setting

---
+++ gatsby-meta-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: `OscarMoon`,
+  description: `Blog posted about ...`,
+  author: `OscarMoon`,
+  introduction: `Hello World! This is Oscar Moon Dev Blogs.`,
+  siteUrl: `https://oscarmoon5256@github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `https://github.com/OscarMoon5256`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+
+}
